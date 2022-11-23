d277a73c998e6446fd0f60eb66934f30d047d034
Thamires Vitoria <thamiresv394@gmail.com>
2022-11-23 11:05:49 -0300

    Movimentação do Personagem

---
+++ Unity/TCC/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float velocity = 10.0f;
+    public float rotation = 90.0f;
+
+    void Start(){
+          Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 dir = new Vector3(x, 0, y) * velocity;
+
+        transform.Translate(dir * Time.deltaTime);
+        transform.Rotate(new Vector3(0, mouseX * rotation* 10 * Time.deltaTime, 0));
+    }
+}
